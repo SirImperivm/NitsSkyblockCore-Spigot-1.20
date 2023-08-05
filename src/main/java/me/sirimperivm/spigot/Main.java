7dfdcd5c01b606ebd147b6b2328098add494a558
SirImperivm <riccardo.chimento24@gmail.com>
2023-08-05 10:35:29 +0200

    Primo upload.

---
+++ src/main/java/me/sirimperivm/spigot/Main.java
@@ -0,0 +1,18 @@
+package me.sirimperivm.spigot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("all")
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
