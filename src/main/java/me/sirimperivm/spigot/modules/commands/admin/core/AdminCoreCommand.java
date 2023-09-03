0f8b683db06be38fa91cdf89925e6cc9118717d9
BuildTools <unconfigured@null.spigotmc.org>
2023-09-03 19:36:33 +0200

    Changelog:   - Inserito customMessageSender.

--- src/main/java/me/sirimperivm/spigot/modules/commands/admin/core/AdminCoreCommand.java
+++ src/main/java/me/sirimperivm/spigot/modules/commands/admin/core/AdminCoreCommand.java
@@ -2,11 +2,15 @@ package me.sirimperivm.spigot.modules.commands.admin.core;
 
 import me.sirimperivm.spigot.Main;
 import me.sirimperivm.spigot.assets.managers.Config;
+import me.sirimperivm.spigot.assets.managers.Db;
+import me.sirimperivm.spigot.assets.managers.Modules;
 import me.sirimperivm.spigot.assets.utils.Colors;
 import me.sirimperivm.spigot.assets.utils.Errors;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 @SuppressWarnings("all")
 public class AdminCoreCommand implements CommandExecutor {
@@ -16,6 +20,8 @@ public class AdminCoreCommand implements CommandExecutor {
         plugin.log(value);
     }
     private static Config conf = Main.getConf();
+    private static Modules mods = Main.getMods();
+    private static Db data = Main.getData();
 
     private void getUsage(CommandSender s) {
         for (String usage : conf.getHelps().getStringList("helps.admin-commands.core")) {
@@ -42,6 +48,25 @@ public class AdminCoreCommand implements CommandExecutor {
                         }
                     } else {
                         getUsage(s);
+                    }
+                } else if (a.length == 3) {
+                    if (a[0].equalsIgnoreCase("sendmessage")) {
+                        if (Errors.noPermCommand(s, conf.getSettings().getString("permissions.admin-commands.core.send-message"))) {
+                            return true;
+                        } else {
+                            if (mods.getModulesInfo().get("message-sender")) {
+                                Player t = Bukkit.getPlayerExact(a[1]);
+                                if (t == null) {
+                                    s.sendMessage(Config.getTransl("settings", "messages.errors.admin.message-sender.target-not-found")
+                                            .replace("${player}", a[1]));
+                                } else {
+                                    String message = a[2].replace("_", " ");
+                                    t.sendMessage(Colors.text(message));
+                                }
+                            } else {
+                                s.sendMessage(Config.getTransl("settings", "messages.errors.general.module-disabled"));
+                            }
+                        }
                     }
                 } else {
                     getUsage(s);
--- src/main/java/me/sirimperivm/spigot/modules/tabCompleters/AdminCoreTabCompleter.java
+++ src/main/java/me/sirimperivm/spigot/modules/tabCompleters/AdminCoreTabCompleter.java
@@ -2,9 +2,12 @@ package me.sirimperivm.spigot.modules.tabCompleters;
 
 import me.sirimperivm.spigot.Main;
 import me.sirimperivm.spigot.assets.managers.Config;
+import me.sirimperivm.spigot.assets.managers.Modules;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +20,7 @@ public class AdminCoreTabCompleter implements TabCompleter {
         plugin.log(value);
     }
     private static Config conf = Main.getConf();
+    private static Modules mods = Main.getMods();
 
     @Override
     public List<String> onTabComplete(CommandSender s, Command c, String l, String[] a) {
@@ -26,6 +30,23 @@ public class AdminCoreTabCompleter implements TabCompleter {
                 if (a.length == 1) {
                     List<String> toReturn = new ArrayList<>();
                     toReturn.add("reload");
+                    toReturn.add("sendmessage");
+                    return toReturn;
+                }
+                else if (a.length == 2) {
+                    List<String> toReturn = new ArrayList<>();
+                    if (a[0].equalsIgnoreCase("sendmessage") && mods.getModulesInfo().get("message-sender")) {
+                        for (Player p : Bukkit.getOnlinePlayers()) {
+                            toReturn.add(p.getName());
+                        }
+                    }
+                    return toReturn;
+                }
+                else if (a.length >= 3) {
+                    List<String> toReturn = new ArrayList<>();
+                    if (a[0].equalsIgnoreCase("sendmessage") && mods.getModulesInfo().get("message-sender")) {
+                        toReturn.add("<tuo_messaggio>");
+                    }
                     return toReturn;
                 }
             }
